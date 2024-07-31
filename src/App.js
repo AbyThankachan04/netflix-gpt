1581f914750b6f8648ece30979480f845f182ca1
Aby Thankachan <taby0406@gmail.com>
2024-07-31 05:53:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="font-bold"> hello world netflix</div>
+    </div>
+  );
+}
+
+export default App;
